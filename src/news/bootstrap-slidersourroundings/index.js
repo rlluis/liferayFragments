00f17f18dc8b7671d5445549dff066788373d016
rlluis <rafael.lluis@liferay.com>
2025-06-08 11:18:40 +0200

    Adjusting Edit Mode for Carousel

--- src/news/bootstrap-slidersourroundings/index.js
+++ src/news/bootstrap-slidersourroundings/index.js
@@ -5,8 +5,16 @@ $( document ).ready(function() {
 		for (var i=0;i<mySize;i++){
 			$(".mycarousel-item")[i].parentNode.parentNode.className += " carousel-item " + active;
 			active="";
+		}		
+	}else{
+		setTimeout(function(){
+		var mySize=$(".mycarousel-item").length;
+		var active="active";
+		for (var i=0;i<mySize;i++){		
+			$(".mycarousel-item")[i].closest('.align-items-undefined').className += " carousel-item " + active;
+			active="";
 		}
-		
+		}, 2000);	
 	}
-	}
+}
 )
