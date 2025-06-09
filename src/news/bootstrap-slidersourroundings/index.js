191668335636ed83db55393dfd3eb12e5f786c0a
rlluis <rafael.lluis@liferay.com>
2025-06-09 14:24:07 +0200

    More Carousel Adjustments, stop TimeOut, welcome MutationObserver

--- src/news/bootstrap-slidersourroundings/index.js
+++ src/news/bootstrap-slidersourroundings/index.js
@@ -1,3 +1,29 @@
+var llamado=false;
+const observer = new MutationObserver((mutationsList, observer) => {
+    for(const mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+					const nodes = mutation.addedNodes;
+          nodes.forEach(node => {
+                if (node.className.includes('row')&&(!llamado)){
+									llamado=true;
+									initializeSlider();
+								}
+          });
+				
+				}
+    }
+}); 
+
+function initializeSlider(){
+		var mySize=$(".mycarousel-item").length;
+		var active="active";
+		for (var i=0;i<mySize;i++){		
+			$(".mycarousel-item")[i].closest('.align-items-undefined').className += " carousel-item " + active;
+			active="";
+		}
+}
+
+
 $( document ).ready(function() {	
 	if (layoutMode=='view'){
 		var mySize=$(".mycarousel-item").length;
@@ -7,14 +33,12 @@ $( document ).ready(function() {
 			active="";
 		}		
 	}else{
-		setTimeout(function(){
-		var mySize=$(".mycarousel-item").length;
-		var active="active";
-		for (var i=0;i<mySize;i++){		
-			$(".mycarousel-item")[i].closest('.align-items-undefined').className += " carousel-item " + active;
-			active="";
-		}
-		}, 2000);	
+		const elementNode = document.querySelector('.carousel-inner').querySelector('.lfr-layout-structure-item-collection');
+
+		observer.observe(elementNode, { 
+    	attributes: false, 
+    	childList: true, 
+    	subtree: false }
+		)
 	}
-}
-)
+})
