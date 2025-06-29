6555b87780790134342d5b82a5deb0f3510c5b0c
rlluis <rafael.lluis@liferay.com>
2025-06-29 12:50:52 +0200

    Little improvements to freemarker and JS code

--- src/news/bootstrap-slidersourroundings/index.js
+++ src/news/bootstrap-slidersourroundings/index.js
@@ -1,45 +1,72 @@
-var llamado=false;
-const observer = new MutationObserver((mutationsList, observer) => {
-    for(const mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-					const nodes = mutation.addedNodes;
-          nodes.forEach(node => {
-                if (node.className.includes('row')&&(!llamado)){
-									llamado=true;
-									initializeSlider();
-								}
-          });
-				
-				}
-    }
-}); 
+let sliderInitialized = false;
 
-function initializeSlider(){
-		var mySize=$("#" + fragmentElement.id + " .mycarousel-item").length;
-		var active="active";
-		for (var i=0;i<mySize;i++){		
-			$("#" + fragmentElement.id + " .mycarousel-item")[i].closest('.align-items-undefined').className += " carousel-item " + active;
-			active="";
+function initializeSlider() {
+	const items = document.querySelectorAll(
+		`#${fragmentElement.id} .lfr-layout-structure-item-bootstrap-slideritem`
+	);
+	let active = 'active';
+
+	items.forEach((item) => {
+		const slideContainer = item.parentNode?.parentNode;
+		if (slideContainer?.classList) {
+			slideContainer.classList.add('carousel-item');
+			if (active) {
+				slideContainer.classList.add(active);
+				active = '';
+			}
 		}
+	});
+
+	// Explicitly initialize the Bootstrap 4.6.2 carousel behavior
+	$(`#${fragmentElement.id} .carousel`).carousel({
+		interval: 3000,
+		ride: 'carousel'
+	});
 }
 
-
-$( document ).ready(function() {	
-	if (layoutMode=='view'){
-		var mySize=$("#" + fragmentElement.id + " .mycarousel-item").length;
-		var active="active";
-		for (var i=0;i<mySize;i++){
-			$("#" + fragmentElement.id + " .mycarousel-item")[i].parentNode.parentNode.className += " carousel-item " + active;
-			active="";
-		}		
-	}else{
-
-		const elementNode = fragmentElement.querySelector('.carousel-inner').querySelector('.lfr-layout-structure-item-collection');
-
-		observer.observe(elementNode, { 
-    	attributes: false, 
-    	childList: true, 
-    	subtree: false }
-		)
+function setupObserver() {
+	const collectionNode = fragmentElement.querySelector(
+		'.carousel-inner .lfr-layout-structure-item-collection'
+	);
+	if (collectionNode) {
+		const observer = new MutationObserver((mutationsList) => {
+			for (const mutation of mutationsList) {
+				if (mutation.type === 'childList') {
+					for (const node of mutation.addedNodes) {
+						if (
+							node.nodeType === Node.ELEMENT_NODE &&
+							node.classList.contains('row') &&
+							!sliderInitialized
+						) {
+							sliderInitialized = true;
+							initializeSlider();
+							break;
+						}
+					}
+				}
+			}
+		});
+		observer.observe(collectionNode, {
+			childList: true,
+			subtree: false
+		});
 	}
-})
+}
+
+function init() {
+	sliderInitialized = false;
+
+	if (layoutMode === 'view') {
+		initializeSlider();
+	} else {
+		setupObserver();
+	}
+}
+
+// Initial load
+init();
+
+// Re-run after each SennaJS navigation
+Liferay.on('endNavigate', () => {
+	init();
+});
